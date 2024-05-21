faea7e3a0f5546ea9793a645cc55a523f82df2a2
kxzb.fun <sunday_ever@163.com>
2024-05-22 07:45:38 +0800

    feat: use express Router controll the route

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const app = express();
 const cors = require("cors");
 const logger = require("./utils/logger.js");
 const Blog = require("./models/blog.js");
-
+const blogsRouter = require('./controllers/blogs.js')
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
+blogsRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
     // logger.info(blogs[0]._id)
   });
 });
 
-app.post("/api/blogs", (request, response) => {
+blogsRouter.post("/", (request, response) => {
   const blog = new Blog(request.body);
 
   blog.save().then((result) => {
     response.status(201).json(result);
-  });
+  }).catch(error=>console.log(error))
 });
 
+app.use('/api/blogs', blogsRouter)
 module.exports = app;
